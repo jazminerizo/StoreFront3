a151e1425a9116003f0ce396e82a311b883dd95c
jazminerizo <97970995+jazminerizo@users.noreply.github.com>
2022-03-28 20:56:45 -0500

    updated scripts bundle

--- StoreFront3.UI.MVC/App_Start/BundleConfig.cs
+++ StoreFront3.UI.MVC/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace StoreFront3.UI.MVC
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/js/scripts.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
